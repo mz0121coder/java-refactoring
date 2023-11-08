bb060486a80e60e2377c7379fff75068aa458ef5
Mohammad Zafar <mzcoder7@gmail.com>
2023-11-08 18:33:18 +0000

    Refactor cash register & test code

--- src/main/java/com/bob/BobsAutoRegister.java
+++ src/main/java/com/bob/BobsAutoRegister.java
@@ -1,51 +1,56 @@
 package com.bob;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BobsAutoRegister {
+    public static void main(String[] args) {
+        List<Product> order = parseOrder(args);
+        Map<Product, Integer> productCount = getProductCount(order);
+        double orderTotal = getTotalForOrder(order);
+        String orderReceipt = getReceiptForOrder(productCount, orderTotal);
+        System.out.println(orderReceipt);
+    }
 
-	public static void main(String[] args) {
+    static protected List<Product> parseOrder(String[] args) {
+        List<Product> order = new ArrayList<>();
+        for (String arg : args) {
+            order.add(Product.valueOf(arg));
+        }
+        return order;
+    }
 
+    static protected Map<Product, Integer> getProductCount(List<Product> order) {
+        Map<Product, Integer> productCount = new HashMap<>();
+        for (Product product : order) {
+            productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+        }
+        return productCount;
+    }
 
-		List<Product> order = parseOrder(args);
+    static protected double getTotalForOrder(List<Product> order) {
+        double totalPrice = 0.0;
+        for (Product item : order) {
+            totalPrice += item.getPrice();
+        }
+        return totalPrice;
+    }
 
-		double orderTotal = getTotalForOrder(order);
-
-		String orderReceipt = getReceiptForOrder(order, orderTotal);
-
-		System.out.println(orderReceipt);
-	}
-
-
-	static protected List<Product> parseOrder(String[] args) {
-		List<Product> order = new ArrayList<Product>();
-		for (int i = 0; i < args.length; i++) {
-			order.add(Product.valueOf(args[i]));
-		}
-		return order;
-	}
-
-	static protected double getTotalForOrder(List<Product> order) {
-		double totalPrice = 0.0;
-
-		for (Product item : order) {
-			totalPrice += item.getPrice();
-		}
-
-		return totalPrice;
-	}
-
-	static protected String getReceiptForOrder(List<Product> order,
-			double orderTotal) {
-		StringBuilder receipt = new StringBuilder();
-
-		for (Product product : order) {
-			receipt.append(String.format("1 %s : £%.2f\n", product.name(), product.getPrice()));
-		}
-
-		receipt.append(String.format("GRAND TOTAL : £%.2f\n", orderTotal));
-
-		return receipt.toString();
+		static protected String getReceiptForOrder(Map<Product, Integer> productCount, double orderTotal) {
+			StringBuilder receipt = new StringBuilder();
+			List<Map.Entry<Product, Integer>> sortedProductCount = new ArrayList<>(productCount.entrySet());
+			sortedProductCount.sort(Comparator.comparing(Map.Entry::getKey));
+	
+			for (Map.Entry<Product, Integer> entry : sortedProductCount) {
+					Product product = entry.getKey();
+					int count = entry.getValue();
+					double productTotal = product.getPrice() * count;
+					receipt.append(String.format("%d %s @ £%.2f : £%.2f\n", count, product.name(), product.getPrice(), productTotal));
+			}
+			receipt.append(String.format("GRAND TOTAL : £%.2f\n", orderTotal));
+			return receipt.toString();
 	}
 }
--- src/test/java/com/bob/BobsAutoRegisterTest.java
+++ src/test/java/com/bob/BobsAutoRegisterTest.java
@@ -6,91 +6,94 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class BobsAutoRegisterTest {
 
-	private static final Product[] TEST_ORDER_1 = {
-		Product.CAR_WASH_WITH_WAX };
+    private static final Product[] TEST_ORDER_1 = {
+            Product.CAR_WASH_WITH_WAX
+    };
 
-	private static final Product[] TEST_ORDER_2 = {
-		Product.CAR_WASH_WITH_WAX,
-		Product.VALET
-	};
+    private static final Product[] TEST_ORDER_2 = {
+            Product.CAR_WASH_WITH_WAX,
+            Product.VALET
+    };
 
-	private static final Product[] TEST_ORDER_3 = {
-		Product.CAR_WASH_NORMAL,
-		Product.SERVICING
-	};
+    private static final Product[] TEST_ORDER_3 = {
+            Product.CAR_WASH_NORMAL,
+            Product.SERVICING
+    };
 
-	private static final Product[] TEST_ORDER_4 = {
-		Product.SERVICING,
-		Product.VALET,
-		Product.VALET
-	};
+    private static final Product[] TEST_ORDER_4 = {
+            Product.SERVICING,
+            Product.VALET,
+            Product.VALET
+    };
 
-	@Before
-	public void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	}
+    @Test
+    public void testParseOrder() {
+        List<Product> expectedProducts = Arrays.asList(TEST_ORDER_1);
+        List<Product> products = BobsAutoRegister.parseOrder(new String[]{"CAR_WASH_WITH_WAX"});
+        assertEquals(expectedProducts, products);
 
-	@Test
-	public void testParseOrder() {
-		List<Product> expectedProducts = Arrays.asList(TEST_ORDER_1);
-		List<Product> products = BobsAutoRegister.parseOrder(new String[]{"CAR_WASH_WITH_WAX"});
+        expectedProducts = Arrays.asList(TEST_ORDER_2);
+        products = BobsAutoRegister.parseOrder(new String[]{"CAR_WASH_WITH_WAX", "VALET"});
+        assertEquals(expectedProducts, products);
 
-		assertEquals(expectedProducts, products);
+        expectedProducts = Arrays.asList(TEST_ORDER_3);
+        products = BobsAutoRegister.parseOrder(new String[]{"CAR_WASH_NORMAL", "SERVICING"});
+        assertEquals(expectedProducts, products);
 
-		expectedProducts = Arrays.asList(TEST_ORDER_2);
-		products = BobsAutoRegister.parseOrder(new String[]{"CAR_WASH_WITH_WAX", "VALET"});
+        expectedProducts = Arrays.asList(TEST_ORDER_4);
+        products = BobsAutoRegister.parseOrder(new String[]{"SERVICING", "VALET", "VALET"});
+        assertEquals(expectedProducts, products);
+    }
 
-		expectedProducts = Arrays.asList(TEST_ORDER_3);
-		products = BobsAutoRegister.parseOrder(new String[]{"CAR_WASH_NORMAL", "SERVICING"});
+    @Test
+    public void testGetTotalForOrder() {
+        List<Product> order = new ArrayList<>();
+        assertEquals(0.0, BobsAutoRegister.getTotalForOrder(order), 0);
 
-		expectedProducts = Arrays.asList(TEST_ORDER_4);
-		products = BobsAutoRegister.parseOrder(new String[]{"SERVICING", "VALET", "VALET"});
+        order.add(Product.CAR_WASH_WITH_WAX);
+        assertEquals(18.0, BobsAutoRegister.getTotalForOrder(order), 0);
 
-		assertEquals(expectedProducts, products);
-	}
+        order.add(Product.SERVICING);
+        order.add(Product.VALET);
+        assertEquals(143.0, BobsAutoRegister.getTotalForOrder(order), 0);
 
-	@Test
-	public void testGetTotalForOrder() {
-		List<Product> order = new ArrayList<Product>();
-		assertEquals(0.0, BobsAutoRegister.getTotalForOrder(order), 0);
+        order.add(Product.SERVICING);
+        assertEquals(243.0, BobsAutoRegister.getTotalForOrder(order), 0);
 
-		order.add(Product.CAR_WASH_WITH_WAX);
-		assertEquals(18.0, BobsAutoRegister.getTotalForOrder(order), 0);
+        assertEquals(188.0, BobsAutoRegister.getTotalForOrder(Arrays.asList(Product.values())), 0);
+    }
 
-		order.add(Product.SERVICING);
-		order.add(Product.VALET);
-		assertEquals(143.0, BobsAutoRegister.getTotalForOrder(order), 0);
+    @Test
+    public void testGetReceiptForOrder() {
+        String expectedReceipt =
+                "1 CAR_WASH_WITH_WAX @ £18.00 : £18.00\n" +
+                        "GRAND TOTAL : £18.00\n";
 
-		order.add(Product.SERVICING);
-		assertEquals(243.0, BobsAutoRegister.getTotalForOrder(order), 0);
+        List<Product> order = Arrays.asList(TEST_ORDER_1);
+        double orderTotal = BobsAutoRegister.getTotalForOrder(order);
+        Map<Product, Integer> productCount = BobsAutoRegister.getProductCount(order);
+        String receiptForOrder = BobsAutoRegister.getReceiptForOrder(productCount, orderTotal);
+        assertEquals(expectedReceipt, receiptForOrder);
 
-		Arrays.asList(Product.values());
-		assertEquals(188.0, BobsAutoRegister.getTotalForOrder(Arrays.asList(Product.values())), 0);
-	}
+        expectedReceipt =
+                "1 SERVICING @ £100.00 : £100.00\n" +
+                        "2 VALET @ £25.00 : £50.00\n" +
+                        "GRAND TOTAL : £150.00\n";
 
-	@Test
-	public void testGetReceiptForOrder() {
-		String expectedReceipt =
-			"1 CAR_WASH_WITH_WAX : £18.00\n" +
-			"GRAND TOTAL : £18.00\n";
-
-		List<Product> order = Arrays.asList(TEST_ORDER_1);
-		String receiptForOrder = BobsAutoRegister.getReceiptForOrder(order, BobsAutoRegister.getTotalForOrder(order));
-		assertEquals(expectedReceipt, receiptForOrder);
-
-
-		expectedReceipt = "1 SERVICING : £100.00\n" +
-						   "1 VALET : £25.00\n" +
-						   "1 VALET : £25.00\n" +
-						   "GRAND TOTAL : £150.00\n";
-		order = Arrays.asList(TEST_ORDER_4);
-		receiptForOrder = BobsAutoRegister.getReceiptForOrder(order, BobsAutoRegister.getTotalForOrder(order));
-		assertEquals(expectedReceipt, receiptForOrder);
-	}
-
+        order = Arrays.asList(TEST_ORDER_4);
+        orderTotal = BobsAutoRegister.getTotalForOrder(order);
+        productCount = BobsAutoRegister.getProductCount(order);
+        receiptForOrder = BobsAutoRegister.getReceiptForOrder(productCount, orderTotal);
+        assertEquals(expectedReceipt, receiptForOrder);
+    }
 }
